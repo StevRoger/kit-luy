c40ef5b224876af4c41e34bcb05a8ffe2dca5e09
PhollaDol <dolpholla168@gmail.com>
2021-01-30 19:43:38 +0700

    add component and config route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomePageComponent },
+  { path: "about", component: AboutUsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutUsPageComponent,
   ],
   imports: [
     BrowserModule,
